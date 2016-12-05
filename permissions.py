b1439498301413de8296d1007ee5cbf215497493
Unilks <tank357@icloud.com>
2016-12-05 14:15:14 +0800

    准备修改权限类

--- permissions.py
+++ permissions.py
@@ -17,22 +17,3 @@ class Base():
         "基本delete权限"
         return True
 
-
-class test_permission(Base):
-    """测试权限，只开放get接口"""
-
-    def get(self, db_session, object, request, pk):
-        "无 get 权限"
-        return True
-
-    def put(self, db_session, object, request, pk):
-        "无 put 权限"
-        return False
-
-    def post(self, db_session, object, request):
-        "无 post 权限"
-        return False
-
-    def delete(self, db_session, object, request, pk):
-        "无 delete 权限"
-        return False
