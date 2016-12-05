fbc9c7b91364e6310944e7714accef02c4746c72
Unilks <tank357@icloud.com>
2016-12-05 14:36:41 +0800

    修改indent错误

--- Model/Order.py
+++ Model/Order.py
@@ -7,7 +7,7 @@ from Base import db_session, Base
 
 
 class Order(Base, Dynamic_permission):
-	"""订单类，提供商品和溯源信息	"""
+    """订单类，提供商品和溯源信息	"""
     db_session = db_session
     __tablename__ = 'Order'
     id = Column(Integer, primary_key=True)
