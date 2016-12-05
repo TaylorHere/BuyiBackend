c7f6f6357a74d7622708c9a557b45a1c4894433c
Unilks <tank357@icloud.com>
2016-12-05 14:34:15 +0800

    增加utf-8编码

--- Model/Order.py
+++ Model/Order.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from sqlalchemy import Column, Integer, String, Text, Boolean, Float, DateTime, Enum, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -6,7 +7,7 @@ from Base import db_session, Base
 
 
 class Order(Base, Dynamic_permission):
-	"""订单类，提供商品和溯源信息"""
+	"""订单类，提供商品和溯源信息	"""
     db_session = db_session
     __tablename__ = 'Order'
     id = Column(Integer, primary_key=True)
